8143d0efaa68cea7744f6990f710dc85e6a057cc
Arturo Seoane Liberata <aseoaneliberata@unomaha.edu>
2017-04-19 14:15:48 -0500

    Map class modification
    rearranging the process to get started this afternoon with the test
    dungeon we talked about this week.
    
    This class implementation will allow people to work in the Map and in
    the SpriteDemo class separately, creating less conflicts between
    workstations.

--- GoldRush/src/Map.java
+++ GoldRush/src/Map.java
@@ -1,18 +1,19 @@
-// Hello test
 public class Map {
+
 	public Background background;
 	public WarpTile warpTile;
 	public Collectible collectible;
 
-	RoomData[] roomData;
+	public RoomData[] roomData;
 
-	
 	public Map() {
-
+		background = new Background(0f, 0f);
+		roomData = new RoomData[] { new RoomData("Images/Room-0.png"), new RoomData("Images/Room-1.png"),
+				new RoomData("Images/Room-0.png", "QTE") };
 	}
 
-	public void initialize(int appWidth,int appHeight,float appWorldWidth,float appWorldHeight) {
-		background = new Background(0f, 0f);
+	public void initialize(int appWidth, int appHeight, float appWorldWidth, float appWorldHeight) {
+
 		background.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 		background.setSubBox();
 		background.setSprite("thing");
@@ -21,6 +22,9 @@ public class Map {
 		WarpTile s1 = new WarpTile(1, 0, -3f, 0, 4.2f);
 		Collectible c1 = new Collectible(7f, 3f, "Gold", 100);
 		c1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		
+		roomData[0].addWarpTile(s1);
+		roomData[0].addCollectible(c1);
 
 		// Room 1 Tile 0
 		WarpTile s2 = new WarpTile(0, 0, 3f, 0, -4.2f);
@@ -30,21 +34,18 @@ public class Map {
 		c2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 		c3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 
-		// Room 3 Tile 0
-		WarpTile s4 = new WarpTile(1, -7f, 0, 7.7f, 0f);
-		
-		roomData = new RoomData[] { new RoomData("Images/Room-0.png"), new RoomData("Images/Room-1.png"),
-				new RoomData("Images/Room-0.png", "QTE") };
-		roomData[0].addWarpTile(s1);
-		roomData[0].addCollectible(c1);
-
 		roomData[1].addWarpTile(s2);
 		roomData[1].addWarpTile(s3);
 		roomData[1].addCollectible(c2);
 		roomData[1].addCollectible(c3);
 
+		// Room 3 Tile 0
+		WarpTile s4 = new WarpTile(1, -7f, 0, 7.7f, 0f);
+
 		roomData[2].addWarpTile(s4);
 		roomData[2].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+
+		// Extra check?
 		if (roomData[2].db == null) {
 			System.out.println("NULL");
 		}
