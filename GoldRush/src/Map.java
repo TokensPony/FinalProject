abb8113541efbc34d6051cf1c6dd4689c01774f5
Arturo Seoane Liberata <aseoaneliberata@unomaha.edu>
2017-04-19 13:49:08 -0500

    Class Map implemented. Needs more work on modularitzation of Rooms.

--- GoldRush/src/Map.java
+++ GoldRush/src/Map.java
@@ -1,4 +1,53 @@
 
 public class Map {
+	public Background background;
+	public WarpTile warpTile;
+	public Collectible collectible;
 
+	RoomData[] roomData;
+
+	
+	public Map() {
+
+	}
+
+	public void initialize(int appWidth,int appHeight,float appWorldWidth,float appWorldHeight) {
+		background = new Background(0f, 0f);
+		background.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		background.setSubBox();
+		background.setSprite("thing");
+
+		// Room 0 Tile 0
+		WarpTile s1 = new WarpTile(1, 0, -3f, 0, 4.2f);
+		Collectible c1 = new Collectible(7f, 3f, "Gold", 100);
+		c1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+
+		// Room 1 Tile 0
+		WarpTile s2 = new WarpTile(0, 0, 3f, 0, -4.2f);
+		WarpTile s3 = new WarpTile(2, 7f, 0f, -7.8f, 0);
+		Collectible c2 = new Collectible(0f, 0f, "Oxygen", 50);
+		Collectible c3 = new Collectible(-5f, 3f, "Gold", 100);
+		c2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		c3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+
+		// Room 3 Tile 0
+		WarpTile s4 = new WarpTile(1, -7f, 0, 7.7f, 0f);
+		
+		roomData = new RoomData[] { new RoomData("Images/Room-0.png"), new RoomData("Images/Room-1.png"),
+				new RoomData("Images/Room-0.png", "QTE") };
+		roomData[0].addWarpTile(s1);
+		roomData[0].addCollectible(c1);
+
+		roomData[1].addWarpTile(s2);
+		roomData[1].addWarpTile(s3);
+		roomData[1].addCollectible(c2);
+		roomData[1].addCollectible(c3);
+
+		roomData[2].addWarpTile(s4);
+		roomData[2].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		if (roomData[2].db == null) {
+			System.out.println("NULL");
+		}
+
+	}
 }
--- GoldRush/src/SpriteDemo.java
+++ GoldRush/src/SpriteDemo.java
@@ -12,24 +12,25 @@ import javagames.util.WindowFramework;
 //=======
 //BLAH BLAH
 public class SpriteDemo extends WindowFramework {
-//>>>>>>> branch 'master' of https://github.com/TokensPony/FinalProject.git
-	
+	// >>>>>>> branch 'master' of https://github.com/TokensPony/FinalProject.git
+
 	Random r = new Random();
-	
+
 	MarioSprite mario;
 	Background background;
 	HealthBar healthBar;
 	Score score;
-	//WarpTile s1;
-	///arpTile s2;
-	
+	// WarpTile s1;
+	/// arpTile s2;
+	private Map map;
+
 	boolean gameOver = false;
 	boolean controlLock = false;
-	
+
 	int cRoom = 0;
-	
+
 	RoomData[] roomData;
-	
+
 	public SpriteDemo() {
 		appBackground = Color.BLACK;
 		appBorder = Color.LIGHT_GRAY;
@@ -43,111 +44,80 @@ public class SpriteDemo extends WindowFramework {
 		appTitle = "Gold Rush v1.0";
 		appWorldWidth = 16.0f;
 		appWorldHeight = 9.0f;
-		
+
 		// temporal fix. Arturo.
 		setResizable(false);
 	}
-	
-	/*Initializes the various sprites w/ their coordinates and sets their bounding boxes.*/
+
+	/*
+	 * Initializes the various sprites w/ their coordinates and sets their
+	 * bounding boxes.
+	 */
 	@Override
 	protected void initialize() {
 		super.initialize();
-		
-		background = new Background(0f, 0f);
-		background.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		background.setSubBox();
-		background.setSprite("thing");
-		//sprites.add(new Floor(-1.0f, -4.4f));
+
+		// Initialization of the Map, rooms, collectibles, warptiles, etc...
+		map = new Map();
+		map.initialize(appWidth, appHeight, appWorldWidth, appWorldHeight);
+
+		// sprites.add(new Floor(-1.0f, -4.4f));
 		score = new Score();
-		
+		healthBar = new HealthBar();
+
 		mario = new MarioSprite();
 		mario.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 		mario.setSubBox();
-		
-		//Room 0 Tile 0
-		WarpTile s1 = new WarpTile(1, 0, -3f, 0, 4.2f);
-		Collectible c1 = new Collectible(7f, 3f, "Gold", 100);
-		c1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		
-		//Room 1 Tile 0
-		WarpTile s2 = new WarpTile(0, 0, 3f, 0, -4.2f);
-		WarpTile s3 = new WarpTile(2, 7f, 0f, -7.8f, 0);
-		Collectible c2 = new Collectible(0f, 0f, "Oxygen", 50);
-		Collectible c3 = new Collectible(-5f, 3f, "Gold", 100);
-		c2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		
-		//Room 3 Tile 0
-		WarpTile s4 = new WarpTile(1, -7f, 0, 7.7f, 0f);
-		
-		roomData = new RoomData[]{new RoomData("Images/Room-0.png"), new RoomData("Images/Room-1.png"),
-				new RoomData("Images/Room-0.png", "QTE")};
-		roomData[0].addWarpTile(s1);
-		roomData[0].addCollectible(c1);
-		
-		roomData[1].addWarpTile(s2);
-		roomData[1].addWarpTile(s3);
-		roomData[1].addCollectible(c2);
-		roomData[1].addCollectible(c3);
-		
-		roomData[2].addWarpTile(s4);
-		roomData[2].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		if(roomData[2].db == null){
-			System.out.println("NULL");
-		}
-		
-		healthBar = new HealthBar();
-		
 	}
 
-	/*Processes the keyboard input for the various game controls*/
+	/* Processes the keyboard input for the various game controls */
 	@Override
 	protected void processInput(float delta) {
 		super.processInput(delta);
-		if(!controlLock){
-			if(keyboard.keyDown(KeyEvent.VK_W) || keyboard.keyDown(KeyEvent.VK_UP) ){
+		if (!controlLock) {
+			if (keyboard.keyDown(KeyEvent.VK_W) || keyboard.keyDown(KeyEvent.VK_UP)) {
 				mario.setVY(2f);
-			}else if(keyboard.keyDown(KeyEvent.VK_S) || keyboard.keyDown(KeyEvent.VK_DOWN) ){
-				mario.setVY(-2f);	
-			}else{
+			} else if (keyboard.keyDown(KeyEvent.VK_S) || keyboard.keyDown(KeyEvent.VK_DOWN)) {
+				mario.setVY(-2f);
+			} else {
 				mario.setVY(0);
 			}
-			
-			if(keyboard.keyDown(KeyEvent.VK_A) || keyboard.keyDown(KeyEvent.VK_LEFT) ){
+
+			if (keyboard.keyDown(KeyEvent.VK_A) || keyboard.keyDown(KeyEvent.VK_LEFT)) {
 				mario.setVX(-2f);
-			}else if(keyboard.keyDown(KeyEvent.VK_D) || keyboard.keyDown(KeyEvent.VK_RIGHT) ){
+			} else if (keyboard.keyDown(KeyEvent.VK_D) || keyboard.keyDown(KeyEvent.VK_RIGHT)) {
 				mario.setVX(2f);
-			}else{
+			} else {
 				mario.setVX(0);
 			}
-		}else{
+		} else {
 			mario.setVX(0);
 			mario.setVY(0);
 		}
-		
-		if(keyboard.keyDownOnce(KeyEvent.VK_E)){
-			roomData[cRoom].showDB = false;
+
+		if (keyboard.keyDownOnce(KeyEvent.VK_E)) {
+			map.roomData[cRoom].showDB = false;
 			controlLock = false;
 		}
-		
-		if(keyboard.keyDownOnce(KeyEvent.VK_B)){
+
+		if (keyboard.keyDownOnce(KeyEvent.VK_B)) {
 			mario.greenBorder = !mario.greenBorder;
-			background.greenBorder = !background.greenBorder;
+			map.background.greenBorder = !map.background.greenBorder;
 		}
 
-		if(keyboard.keyDownOnce(KeyEvent.VK_1)){
+		if (keyboard.keyDownOnce(KeyEvent.VK_1)) {
 			healthBar.doDamage(10);
 		}
-		
-		if(keyboard.keyDownOnce(KeyEvent.VK_2)){
+
+		if (keyboard.keyDownOnce(KeyEvent.VK_2)) {
 			healthBar.addHealth(50);
 		}
-		
-		if(keyboard.keyDownOnce(KeyEvent.VK_3)){
+
+		if (keyboard.keyDownOnce(KeyEvent.VK_3)) {
 			healthBar.drainOxygen(10);
 		}
-		
-		if(keyboard.keyDownOnce(KeyEvent.VK_4)){
+
+		if (keyboard.keyDownOnce(KeyEvent.VK_4)) {
 			healthBar.addOxygen(50);
 		}
 	}
@@ -155,71 +125,71 @@ public class SpriteDemo extends WindowFramework {
 	@Override
 	protected void updateObjects(float delta) {
 		super.updateObjects(delta);
-		//System.out.printf("%f, %f\n", mario.positions.x, mario.positions.y);
-		//System.out.println(getWorldMousePosition());
-		background.updateObjects(delta);
+		// System.out.printf("%f, %f\n", mario.positions.x, mario.positions.y);
+		// System.out.println(getWorldMousePosition());
+		map.background.updateObjects(delta);
 		mario.updateObjects(delta);
-		//s1.updateObjects(delta);
-		
-		if(mario.rectRectIntersection(background.subBox.get(0).getVWorld(), mario.mainBox.getVWorld())){
+		// s1.updateObjects(delta);
+
+		if (mario.rectRectIntersection(map.background.subBox.get(0).getVWorld(), mario.mainBox.getVWorld())) {
 			mario.positions.y = -3.3f;
-		}else if(mario.rectRectIntersection(background.subBox.get(1).getVWorld(), mario.mainBox.getVWorld())){
+		} else if (mario.rectRectIntersection(map.background.subBox.get(1).getVWorld(), mario.mainBox.getVWorld())) {
 			mario.positions.y = 3.3f;
-		}else if(mario.rectRectIntersection(background.subBox.get(2).getVWorld(), mario.mainBox.getVWorld())){
+		} else if (mario.rectRectIntersection(map.background.subBox.get(2).getVWorld(), mario.mainBox.getVWorld())) {
 			mario.positions.x = -7.2f;
-		}else if(mario.rectRectIntersection(background.subBox.get(3).getVWorld(), mario.mainBox.getVWorld())){
-			//b.setSprite("Thing");
-			//cRoom = roomData[cRoom].wt.get(0).getWarpCoord();
+		} else if (mario.rectRectIntersection(map.background.subBox.get(3).getVWorld(), mario.mainBox.getVWorld())) {
+			// b.setSprite("Thing");
+			// cRoom = map.roomData[cRoom].wt.get(0).getWarpCoord();
 			mario.positions.x = 7.2f;
 		}
-		
-		if(roomData[cRoom].showDB){
+
+		if (map.roomData[cRoom].showDB) {
 			controlLock = true;
 		}
-		roomData[cRoom].updateRoomData(delta);
-		//This is a test
-		for(int x = 0; x < roomData[cRoom].items.size(); x++){
-			//roomData[cRoom].items.get(x).updateObjects(delta);
-			if(mario.rRI(roomData[cRoom].items.get(x).mainBox)){
-				switch(roomData[cRoom].items.get(x).getType()){
+		map.roomData[cRoom].updateRoomData(delta);
+		// This is a test
+		for (int x = 0; x < map.roomData[cRoom].items.size(); x++) {
+			// map.roomData[cRoom].items.get(x).updateObjects(delta);
+			if (mario.rRI(map.roomData[cRoom].items.get(x).mainBox)) {
+				switch (map.roomData[cRoom].items.get(x).getType()) {
 				case "Oxygen":
-					healthBar.addOxygen(roomData[cRoom].items.get(x).getIncrease());
+					healthBar.addOxygen(map.roomData[cRoom].items.get(x).getIncrease());
 					break;
 				case "Gold":
-					score.increaseScore(roomData[cRoom].items.get(x).getIncrease());
+					score.increaseScore(map.roomData[cRoom].items.get(x).getIncrease());
 					break;
 				default:
 					System.out.print("Unknown Thing");
 					break;
-				//x--;
+				// x--;
 				}
-				roomData[cRoom].items.remove(x);
+				map.roomData[cRoom].items.remove(x);
 			}
 		}
-		
-		//roomData[cRoom].updateRoomData(delta);
-		for(int x = 0; x < roomData[cRoom].wt.size(); x++){
-			//roomData[cRoom].wt.get(x).updateObjects(delta);
-			if(mario.rRI(roomData[cRoom].wt.get(x).tile)){
-				//System.out.println("Warped");
-				mario.positions.x = roomData[cRoom].wt.get(x).getWarpToX();
-				mario.positions.y = roomData[cRoom].wt.get(x).getWarpToY();
-				cRoom = roomData[cRoom].wt.get(x).getWarpMap();
-				//System.out.println(cRoom);
-				//b.pos = cRoom;
-				//b.setSprite("thing");
-				//b.changeSprite(roomData[cRoom].getBG());
-				background.currentSprite = roomData[cRoom].getBG();
-				//roomData[cRoom].wt.get(x).updateObjects(delta);
-				roomData[cRoom].updateRoomData(delta);
+
+		// map.roomData[cRoom].updateRoomData(delta);
+		for (int x = 0; x < map.roomData[cRoom].wt.size(); x++) {
+			// map.roomData[cRoom].wt.get(x).updateObjects(delta);
+			if (mario.rRI(map.roomData[cRoom].wt.get(x).tile)) {
+				// System.out.println("Warped");
+				mario.positions.x = map.roomData[cRoom].wt.get(x).getWarpToX();
+				mario.positions.y = map.roomData[cRoom].wt.get(x).getWarpToY();
+				cRoom = map.roomData[cRoom].wt.get(x).getWarpMap();
+				// System.out.println(cRoom);
+				// b.pos = cRoom;
+				// b.setSprite("thing");
+				// b.changeSprite(map.roomData[cRoom].getBG());
+				map.background.currentSprite = map.roomData[cRoom].getBG();
+				// map.roomData[cRoom].wt.get(x).updateObjects(delta);
+				map.roomData[cRoom].updateRoomData(delta);
 				break;
 			}
 		}
-		
-		if(!controlLock){
+
+		if (!controlLock) {
 			healthBar.update(delta);
 			System.out.println(healthBar.healthLevel);
-			if(healthBar.healthLevel <= 0){
+			if (healthBar.healthLevel <= 0) {
 				gameOver = true;
 			}
 		}
@@ -230,27 +200,27 @@ public class SpriteDemo extends WindowFramework {
 	@Override
 	protected void render(Graphics g) {
 		super.render(g);
-		if(!gameOver){
-			background.render(g, getViewportTransform());
+		if (!gameOver) {
+			map.background.render(g, getViewportTransform());
 			mario.render(g, getViewportTransform());
-			//s1.render(g, getViewportTransform());
-			for(int x = 0; x < roomData[cRoom].wt.size(); x++){
-				roomData[cRoom].wt.get(x).render(g, getViewportTransform());
+			// s1.render(g, getViewportTransform());
+			for (int x = 0; x < map.roomData[cRoom].wt.size(); x++) {
+				map.roomData[cRoom].wt.get(x).render(g, getViewportTransform());
 			}
-			for(int x = 0; x < roomData[cRoom].items.size(); x++){
-				roomData[cRoom].items.get(x).render(g, getViewportTransform());
+			for (int x = 0; x < map.roomData[cRoom].items.size(); x++) {
+				map.roomData[cRoom].items.get(x).render(g, getViewportTransform());
 			}
 			healthBar.render(g, getViewportTransform());
 			score.render(g);
-			if(roomData[cRoom].showDB){
-				roomData[cRoom].db.render(g, getViewportTransform());
+			if (map.roomData[cRoom].showDB) {
+				map.roomData[cRoom].db.render(g, getViewportTransform());
 			}
-		}else{
+		} else {
 			g.setColor(Color.RED);
 			g.drawString("GAME OVER", 640, 360);
 		}
 	}
-	
+
 	@Override
 	protected void terminate() {
 		super.terminate();
