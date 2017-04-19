693ee4d4ad91d6c70e3b445047e9939936e7789e
cdkennell <cdkennell@unomaha.edu>
2017-04-19 15:27:26 -0500

    Moved the rooms around to match the board from monday, added rooms 4 and 5

--- GoldRush/src/Map.java
+++ GoldRush/src/Map.java
@@ -19,6 +19,7 @@ public class Map {
 		background.setSprite("thing");
 
 		// Room 0 Tile 0
+		WarpTile s0 = new WarpTile(10, 7f, 0f, -7.8f, 0, false);//this is for the exit
 		WarpTile s1 = new WarpTile(1, 0, -3f, 0, 4.2f, true);
 		Collectible c1 = new Collectible(7f, 3f, "Gold", 100);
 		c1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
@@ -28,21 +29,31 @@ public class Map {
 
 		// Room 1 Tile 0
 		WarpTile s2 = new WarpTile(0, 0, 3f, 0, -4.2f, true);
-		WarpTile s3 = new WarpTile(2, 7f, 0f, -7.8f, 0, true);
+		WarpTile s3 = new WarpTile(2, -7f, 0, 7.7f, 0f, true);
 		Collectible c2 = new Collectible(0f, 0f, "Oxygen", 50);
 		Collectible c3 = new Collectible(-5f, 3f, "Gold", 100);
 		c2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 		c3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 
 		// Room 2 Tile 0
-		WarpTile s4 = new WarpTile(1, -7f, 0, 7.7f, 0f, false);
-		WarpTile s5 = new WarpTile(3, 7f, 0f, -7.8f, 0, false);
+		WarpTile s4 = new WarpTile(1, 7f, 0f, -7.8f, 0, true);
+		WarpTile s5 = new WarpTile(3, -7f, 0, 7.7f, 0f, true);
 		
 		// Room 3 Tile 0
-		WarpTile s6 = new WarpTile(2, -7f, 0, 7.7f, 0f, false);
+		WarpTile s6 = new WarpTile(2, 7f, 0f, -7.8f, 0, true);
+		WarpTile s7 = new WarpTile(5, 0, -3f, 0, 4.2f, true);
+		WarpTile s8 = new WarpTile(4, -7f, 0, 7.7f, 0f, true);
+		
+		//Room 4 Tile 0
+		WarpTile s9 = new WarpTile(3, 7f, 0f, -7.8f, 0, true);
+		
+		//Room 5 Tile 0
+		WarpTile s10 = new WarpTile(3, 0, 3f, 0, -4.2f, true);
 		
 		roomData = new RoomData[] { new RoomData("Images/Room-0.png"), new RoomData("Images/Room-1.png"),
-				new RoomData("Images/Room-0.png", "QTE"), new RoomData("Images/Room-1.png")};
+				new RoomData("Images/Room-0.png", "QTE"), new RoomData("Images/Room-1.png"), new RoomData("Images/Room-0.png"), 
+				new RoomData("Images/Room-1.png")};
+		roomData[0].addWarpTile(s0);
 		roomData[0].addWarpTile(s1);
 		roomData[0].addCollectible(c1);
 
@@ -56,6 +67,12 @@ public class Map {
 		roomData[2].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 		
 		roomData[3].addWarpTile(s6);
+		roomData[3].addWarpTile(s7);
+		roomData[3].addWarpTile(s8);
+		
+		roomData[4].addWarpTile(s9);
+		
+		roomData[5].addWarpTile(s10);
 
 		// Extra check?
 		if (roomData[2].db == null) {
