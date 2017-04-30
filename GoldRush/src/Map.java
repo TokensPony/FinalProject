1736277457b35503c283f21f974aab79407e75b4
xpriab <bishopbrogan@gmail.com>
2017-04-30 17:02:43 -0500

    added addItems() functions
    added new functions to randomly add items to a room
    you can use a single string corresponding to an item or 
    you can us a string array and it will choose randomly out of the items in the string array
    (I also changed the  "appWidth, appHeight, appWorldWidth, appWorldHeight" situation...)

--- GoldRush/src/Map.java
+++ GoldRush/src/Map.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javagames.util.*;
 
 public class Map {
@@ -5,6 +7,9 @@ public class Map {
 	public Background background;
 	public WarpTile warpTile;
 	public Collectible collectible;
+	
+	private int appWidth, appHeight;
+	private float appWorldWidth, appWorldHeight;
 
 	public RoomData[] roomData;
 	
@@ -23,134 +28,137 @@ public class Map {
 				new LavaRoom("Images/Room-0.png")/*Room 8*/, new RoomData("Images/Room-1.png")/*Room 9*/, new RoomData("Images/Room-0.png")/*Roomn10*/, new QTERoom("Images/Room-0.png", "QTE")/*Roomn11*/, 
 				new RockChallengeRoom("Images/Room-0.png", "FallingRocks")/*Roomn12*/, new RoomData("Images/Room-0.png")/*Roomn13*/, new RockChallengeRoom("Images/Room-0.png", "FallingRocks")/*Room 14*/};
 	}
+	
 
-	public void initialize(int appWidth, int appHeight, float appWorldWidth, float appWorldHeight) {
+	public void addItems(int room, int numItems, String[] choices)
+	{
+		Random r = new Random();
+		
+		for (int i = 0; i < numItems; i++)
+		{
+			float posx = r.nextFloat() * 14 - 7;
+			float posy = r.nextFloat() * 8 - 4;
+			int choice = r.nextInt(choices.length);
+			Collectible c = new Collectible(posx, posy, choices[choice]);
+			c.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+			roomData[room].addCollectible(c);
+		}
+	}
+	
+	//overloaded to handle a single string
+	public void addItems(int room, int numItems, String item)
+	{
+		String[] choices = {item};
+		addItems(room, numItems, choices);
+	}
+	
+	public void setAppVals(int aW, int aH, float aWW, float aWH)
+	{
+		this.appWidth = aW;
+		this.appHeight = aH;
+		this.appWorldHeight = aWH;
+		this.appWorldWidth = aWW;
+	}
+	
+	//temporary fix
+	public void initialize(int appWidth, int appHeight, float appWorldWidth, float appWorldHeight) 
+	{
+		setAppVals(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		initialize();
+		
+	}
+	
+	public void initialize() 
+	{
+		String[] allOxygen = {"Oxygen10", "Oxygen30", "Oxygen50", "Oxygen80", "Oxygen100"};
+		String[] allEdibles = {"SmallPasty", "LargePasty", "Chips"};
+		String[] allGold = {"GoldCoin", "GoldNugget", "GoldBar"};
+				
 
 		background.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 		background.setSubBox();
 		background.setSprite("thing");
+		
+		addItems(5, 1, "Arkenstone");
 
-		// Room 0 Tile 0
-		WarpTile s0 = new WarpTile(10, 7f, 0f, -7.8f, 0, false);//this is for the exit
-		WarpTile s1 = new WarpTile(1, 0, -3f, 0, 4.2f, true);
+		/*Room 0*/
+		roomData[0].addWarpTile(new WarpTile(10, 7f, 0f, -7.8f, 0, false));//this is for the exit
+		roomData[0].addWarpTile(new WarpTile(1, 0, -3f, 0, 4.2f, true));
 
-		roomData[0].addWarpTile(s1);
 
-		// Room 1 Tile 0
-		WarpTile s2 = new WarpTile(0, 0, 3f, 0, -4.2f, true);
-		WarpTile s3 = new WarpTile(2, -7f, 0, 7.7f, 0f, true);
-		Collectible c1 = new Collectible(0.2f, 0.2f, "GoldCoin");
-		Collectible c2 = new Collectible(-0.2f, -0.2f, "GoldCoin");
-		Collectible c3 = new Collectible(0.2f, -0.2f, "GoldCoin");
-		Collectible c4 = new Collectible(-0.2f, 0.2f, "GoldCoin");
-		Collectible c5 = new Collectible(2f, 2f, "GoldNugget");
-		Collectible c6 = new Collectible(-2f, -2f, "GoldBar");
-		c1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c4.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c5.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c6.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		/* Room 1 (Gold Demo Room)*/
+		roomData[1].addWarpTile(new WarpTile(0, 0, 3f, 0, -4.2f, true));
+		roomData[1].addWarpTile(new WarpTile(2, -7f, 0, 7.7f, 0f, true));
+		roomData[1].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		addItems(1, 50, "GoldCoin");//put 50 GoldCoins in room 1 randomly
+		addItems(1, 3, "GoldNugget");//put 3 GoldNuggets in room 1
+		addItems(1, 2, "GoldBar");//add 2 GoldBars
+		addItems(1, 1, "Diamond");//add 1 Diamond
+		
+		
+		/* Room 2 Oxygen Demo */
+		roomData[2].addWarpTile(new WarpTile(1, 7f, 0f, -7.8f, 0, true));
+		roomData[2].addWarpTile(new WarpTile(3, -7f, 0, 7.7f, 0f, true));
+		roomData[2].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		addItems(2, 3, allOxygen); //add three items from oxygen[] randomly
 
-		// Room 2 Tile 0
-		WarpTile s4 = new WarpTile(1, 7f, 0f, -7.8f, 0, true);
-		WarpTile s5 = new WarpTile(3, -7f, 0, 7.7f, 0f, true);
-		Collectible o1 = new Collectible(0f, 0f, "Oxygen10");
-		Collectible o2 = new Collectible(-5f, 3f, "Oxygen10");
-		Collectible o3 = new Collectible(6f, 2f, "Oxygen30");
-		Collectible o4 = new Collectible(6f, -3f, "Oxygen10");
-		o1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		o2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		o3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		o4.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-
-		// Room 3 Tile 0
-		WarpTile s6 = new WarpTile(2, 7f, 0f, -7.8f, 0, true);
-		WarpTile s7 = new WarpTile(10, 0, -3f, 0, 4.2f, true);
+		
+		/* Room 3 Branching Paths*/
+		roomData[3].addWarpTile(new WarpTile(2, 7f, 0f, -7.8f, 0, true));
+		roomData[3].addWarpTile(new WarpTile(10, 0, -3f, 0, 4.2f, true));
 		WarpTile s8 = new WarpTile(4, -7f, 0, 7.7f, 0f, true);
 		s8.challengeEntrance = true;
+		roomData[3].addWarpTile(s8);
+		roomData[3].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 
-		// Room 4 Tile 0
-		WarpTile s9 = new WarpTile(3, 7f, 0f, -7.8f, 0, false);
-		WarpTile s11 = new WarpTile(6, 0, -3f, 0, 4.2f, false);
-		Collectible g1 = new Collectible(6f, -3f, "GoldCoin");
-		Collectible g2 = new Collectible(0f, -2f, "GoldBar");
-		Collectible g3 = new Collectible(-7f, 3f, "GoldCoin");
-		Collectible g4 = new Collectible(7f, 3.5f, "Diamond");
-		Collectible g5 = new Collectible(-7f, -3.5f, "GoldNugget");
-		Collectible g6 = new Collectible(4f, 0f, "GoldBar");
-		Collectible o8 = new Collectible(-5.5f, 3f, "Oxygen20");
-		Collectible h2 = new Collectible(0f, -3f, "SmallPasty");
-		Collectible h3 = new Collectible(-3.2f, -2f, "Chips");
-		g1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		g2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		g3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		g4.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		g5.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		g6.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		o8.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		h2.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		h3.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 
-		// Room 5 Tile 0
-		WarpTile s10 = new WarpTile(10, 0, 3f, 0, -4.2f, true);
-		WarpTile s15 = new WarpTile(14, 0, -3f, 0, 4.2f, true);
+		/* Room 4 Falling Rock Demo*/
+		roomData[4].addWarpTile(new WarpTile(3, 7f, 0f, -7.8f, 0, false));
+		roomData[4].addWarpTile(new WarpTile(6, 0, -3f, 0, 4.2f, false));
+		roomData[4].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		roomData[4].setStuff(appWidth, appHeight, appWorldWidth, appWorldHeight);	
+		addItems(4, 1, "Oxygen20");
+		addItems(4, 2, allEdibles);
+		addItems(4, 5, allGold);
+		addItems(4, 1, "Diamond");
 		
-		//Room 6
-		WarpTile s12 = new WarpTile(3, 0, 3f, 0, -4.2f, false);
-		WarpTile s13 = new WarpTile(7, 0, -3f, 0, 4.2f, false);
-				
-		//Room 7
-		WarpTile s14 = new WarpTile(3, 0, 3f, 0, -4.2f, true);
-		Collectible c7 = new Collectible(2f, 2f, "GoldNugget");
-		Collectible c8 = new Collectible(-2f, -2f, "GoldNugget");
-		Collectible c9 = new Collectible(0f, 0f, "GoldNugget");
-		Collectible c10 = new Collectible(-2f, 2f, "GoldNugget");
-		Collectible c11 = new Collectible(2f, 0f, "GoldNugget");
-		Collectible c12 = new Collectible(0f, -2f, "GoldNugget");
-		Collectible c13 = new Collectible(0f, 2f, "GoldNugget");
-		Collectible c14 = new Collectible(3f, 3f, "GoldNugget");
-		Collectible c15 = new Collectible(1f, 3.5f, "GoldBar");
-		Collectible c16 = new Collectible(5f, -1f, "GoldBar");
-		Collectible c17 = new Collectible(2f, 3.5f, "GoldBar");
-		Collectible c18 = new Collectible(0f, -3.5f, "Diamond");
-		Collectible c19 = new Collectible(5f, 0f, "Diamond");
-		Collectible c20 = new Collectible(4f, 3.5f, "Diamond");
-		Collectible c21 = new Collectible(3f, 1f, "Diamond");
-		Collectible c22 = new Collectible(-4f, -3.5f, "Diamond");
-		Collectible h1 = new Collectible(-6f, 0f, "LargePasty");
-		Collectible o5 = new Collectible(-6, 2.5f, "Oxygen50");
-		Collectible o6 = new Collectible(7f, 3.5f, "Oxygen100");
-		Collectible o7 = new Collectible(5f, -2.5f, "Oxygen30");
-		c7.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c8.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c9.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c10.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c11.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c12.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c13.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c14.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c15.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c16.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c17.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c18.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c19.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c20.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c21.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		c22.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		h1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		o5.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		o6.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		o7.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+
+		/* Room 5 First Lava Room*/
+		roomData[5].addWarpTile(new WarpTile(10, 0, 3f, 0, -4.2f, true));
+		roomData[5].setStuff(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		roomData[5].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		roomData[5].addWarpTile(new WarpTile(14, 0, -3f, 0, 4.2f, true));
+		
+		
+		/* Room 6 QTE Demo*/
+		roomData[6].addWarpTile(new WarpTile(3, 0, 3f, 0, -4.2f, false));
+		roomData[6].addWarpTile(new WarpTile(7, 0, -3f, 0, 4.2f, false));
+		roomData[6].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+
+
+		/* Room 7 Pot of Gold*/
+		roomData[7].addWarpTile(new WarpTile(3, 0, 3f, 0, -4.2f, true));
+		addItems(7, 8, "GoldNugget");
+		addItems(7, 3, "GoldBar");
+		addItems(7, 5, "Diamond");
+		addItems(7, 1, "LargePasty");
+		addItems(7, 3, allOxygen);
+
+		//TODO: (Brogan) I need to finish going through the rooms below...
 		
 		//Room 8
 		WarpTile s16 = new WarpTile(14, 0, 4f, 0, -4.2f, true);
 		WarpTile s17 = new WarpTile(9, 0, -3f, 0, 4.2f, true);
+		roomData[8].addWarpTile(s16);
+		roomData[8].setStuff(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		roomData[8].addWarpTile(s17);
 		
 		//Room 9
 		WarpTile s18 = new WarpTile(8, 0, 4f, 0, -4.2f, true);
 		Collectible a1 = new Collectible(0f, 3f, "Arkenstone");
 		a1.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
+		roomData[9].addWarpTile(s18);
+		roomData[9].addCollectible(a1);
 		
 		//Room 10
 		WarpTile s19 = new WarpTile(3, 0, 4f, 0, -4.2f, true);
@@ -241,83 +249,16 @@ public class Map {
 		WarpTile s27 = new WarpTile(5, 0, 4f, 0, -4.2f, true);
 		WarpTile s28 = new WarpTile(8, 0, -3f, 0, 4.2f, true);
 
-		roomData[0].addWarpTile(s0);
-		roomData[0].addWarpTile(s1);
 
-		roomData[1].addWarpTile(s2);
-		roomData[1].addWarpTile(s3);
-		roomData[1].addCollectible(c1);
-		roomData[1].addCollectible(c2);
-		roomData[1].addCollectible(c3);
-		roomData[1].addCollectible(c4);
-		roomData[1].addCollectible(c5);
-		roomData[1].addCollectible(c6);
-		roomData[1].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-
-		roomData[2].addWarpTile(s4);
-		roomData[2].addWarpTile(s5);
-		roomData[2].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		roomData[2].addCollectible(o1);
-		roomData[2].addCollectible(o2);
-		roomData[2].addCollectible(o3);
-		roomData[2].addCollectible(o4);
-
-		roomData[3].addWarpTile(s6);
-		roomData[3].addWarpTile(s7);
-		roomData[3].addWarpTile(s8);
-		roomData[3].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-
-		roomData[4].addWarpTile(s9);
-		roomData[4].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		roomData[4].setStuff(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		roomData[4].addWarpTile(s11);
-		roomData[4].addCollectible(g1);
-		roomData[4].addCollectible(g2);
-		roomData[4].addCollectible(g3);
-		roomData[4].addCollectible(g4);
-		roomData[4].addCollectible(g5);
-		roomData[4].addCollectible(g6);
-		roomData[4].addCollectible(o8);
-		roomData[4].addCollectible(h2);
-		roomData[4].addCollectible(h3);
-
-		roomData[5].addWarpTile(s10);
-		roomData[5].setStuff(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		roomData[5].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		roomData[5].addWarpTile(s15);
 		
-		roomData[6].addWarpTile(s12);
-		roomData[6].addWarpTile(s13);
-		roomData[6].db.setBB(appWidth, appHeight, appWorldWidth, appWorldHeight);
 		
-		roomData[7].addWarpTile(s14);
-		roomData[7].addCollectible(c7);
-		roomData[7].addCollectible(c8);
-		roomData[7].addCollectible(c9);
-		roomData[7].addCollectible(c10);
-		roomData[7].addCollectible(c11);
-		roomData[7].addCollectible(c12);
-		roomData[7].addCollectible(c13);
-		roomData[7].addCollectible(c14);
-		roomData[7].addCollectible(c15);
-		roomData[7].addCollectible(c16);
-		roomData[7].addCollectible(c17);
-		roomData[7].addCollectible(c18);
-		roomData[7].addCollectible(c19);
-		roomData[7].addCollectible(c20);
-		roomData[7].addCollectible(c21);
-		roomData[7].addCollectible(c22);
-		roomData[7].addCollectible(h1);
-		roomData[7].addCollectible(o13);
-		roomData[7].addCollectible(o14);
-		roomData[7].addCollectible(o15);
 		
-		roomData[8].addWarpTile(s16);
-		roomData[8].setStuff(appWidth, appHeight, appWorldWidth, appWorldHeight);
-		roomData[8].addWarpTile(s17);
 		
-		roomData[9].addWarpTile(s18);
-		roomData[9].addCollectible(a1);
+		
+		
+		
+		
+		
 		
 		roomData[10].addWarpTile(s19);
 		roomData[10].addWarpTile(s20);
@@ -365,9 +306,9 @@ public class Map {
 		roomData[13].addCollectible(c37);
 		roomData[13].addCollectible(c38);
 		roomData[13].addCollectible(h8);
-		roomData[13].addCollectible(o5);
-		roomData[13].addCollectible(o6);
-		roomData[13].addCollectible(o7);
+		//roomData[13].addCollectible(o5);
+		//roomData[13].addCollectible(o6);
+		//roomData[13].addCollectible(o7);
 		
 		roomData[14].addWarpTile(s27);
 		roomData[14].setStuff(appWidth, appHeight, appWorldWidth, appWorldHeight);
