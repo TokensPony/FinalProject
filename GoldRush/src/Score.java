0d7555df294ab0e27d14804b87a7c01d1fbdea3d
xpriab <bishopbrogan@gmail.com>
2017-05-01 23:41:34 -0500

    final cleanup (fingers crossed)

--- GoldRush/src/Score.java
+++ GoldRush/src/Score.java
@@ -2,13 +2,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Score{
-	private int score;
-	
+
+	//location to draw score (pixels)
 	private final int scoreX = 100;
 	private final int scoreY = 23;
+	
+	//how bold to draw the score (controls offset in render)
 	private final int boldness = 2;
 	
-	private static String s = "0";
+	private int score;
 	
 	/* Constructor method for the score/Combo */
 	public Score(){
@@ -34,7 +36,7 @@ public class Score{
 	public void render(Graphics g){
 		//This Draws the same string 4 times in offset positions then draws it in
 		//White to make a boarder
-		s = s.format("%d", score);
+		String s = Integer.toString(score);
 		g.setColor(Color.BLACK);
 		g.drawString(s, scoreX+boldness, scoreY);
 		g.drawString(s, scoreX, scoreY+boldness);
